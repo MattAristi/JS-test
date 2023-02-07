c8f4906a65771e878153d099eb9bfc145be886f8
Matias Aristimuno <mattaristi@icloud.com>
2023-02-07 11:47:23 +0000

    clase constructor y metodo

--- js/objetos.js
+++ js/objetos.js
@@ -1,6 +1,6 @@
 function objetosT() {
 
-    let optionObject = doWileCheckNumber(objetosT, 2, '1- Sintaxis y declaracion de objetos', '2- funciones constructoras')
+    let optionObject = doWileCheckNumber(objetosT, 3, '1- Sintaxis y declaracion de objetos', '2- funciones constructoras', '3- Clases, constructores y metodos')
 
     if (optionObject == 1) {
 
@@ -27,38 +27,76 @@ function objetosT() {
         selectOptions()
     }
 
-    if (optionObject == 2){
+    if (optionObject == 2) {
         //funciion constructota
-        function Mueble ([tipo, modelo, material, formato, color, indcar]) {
+        function Mueble([tipo, modelo, material, formato, color, indcar]) {
             (tipo ? this.tipo = tipo : null);
-            (modelo ? this.modelo = modelo.toUpperCase():null);
-            (material ? this.material = material:null);
-            (formato ? this.formato = formato:null);
-            (color ? this.color = color: null);
-            (indcar ? this.indcar=true : null)
-            this.coment = function(){console.log('El modelo '+this.modelo+' es un mueble aprobado por la INDCAR');}
+            (modelo ? this.modelo = modelo.toUpperCase() : null);
+            (material ? this.material = material : null);
+            (formato ? this.formato = formato : null);
+            (color ? this.color = color : null);
+            (indcar ? this.indcar = true : null)
+            this.coment = function () {
+                console.log('El modelo ' + this.modelo + ' es un mueble aprobado por la INDCAR');
+            }
         }
-        let objectMueble; 
+        let objectMueble;
         let arrayMueble;
         do {
-            objectMueble= prompt('Ingresa los datos:\n "tipo, modelo, material, formato, color, Certificado INDCAR en caso de tenerlo"\n En ese orden y separados solo por un espacio entree si.\n Ingresar minimo 5 datos, maximo 6.\n Para salir escribí una x')
+            objectMueble = prompt('Ingresa los datos:\n "tipo, modelo, material, formato, color, Certificado INDCAR en caso de tenerlo"\n En ese orden y separados solo por un espacio entree si.\n Ingresar minimo 5 datos, maximo 6.\n Para salir escribí una x')
             arrayMueble = stringArray(objectMueble)
             console.log(arrayMueble);
             console.log(arrayMueble.length);
-            ( arrayMueble.length != 5 && arrayMueble.length != 6 && arrayMueble[0]!='x' ? alert('Los datos ingresados no son correctos.'): null)
-            
-        } while (arrayMueble.length != 5 && arrayMueble.length != 6 && arrayMueble[0]!='x');
-        
+            (arrayMueble.length != 5 && arrayMueble.length != 6 && arrayMueble[0] != 'x' ? alert('Los datos ingresados no son correctos.') : null)
+
+        } while (arrayMueble.length != 5 && arrayMueble.length != 6 && arrayMueble[0] != 'x');
+
         if (arrayMueble.length == 5 || arrayMueble.length == 6) {
             const mueble1 = new Mueble(arrayMueble)
 
-        console.log(mueble1);
-        if (mueble1.indcar==true){
-        console.log('console log: ',mueble1.coment())
+            console.log(mueble1);
+            if (mueble1.indcar == true) {
+                console.log('console log: ', mueble1.coment())
+            }
         }
-        }
-    selectOptions()
+        selectOptions()
     }
+    if (optionObject == 3) {
+
+        // clases
+
+        //la clase permite tener un constructor y los metodos separados adentro
+
+        //sintaxis
+
+        class Planeta {
+            constructor(nombre, masa, volumen, diametro, gravedad, orbita, color, estrella, satelites) {
+                this.nombre = nombre;
+                this.masa = masa;
+                this.volumen = volumen;
+                this.diametro = diametro;
+                this.gravedad = gravedad;
+                this.orbita = orbita;
+                this.color = color;
+                this.estrella = estrella;
+                this.satelites = satelites;
+            }
+            density() {
+                this.densidad = this.masa / this.volumen
+            }
+        }
+
+        const planeta1 = new Planeta('Tierra', 5.97 * Math.pow(10, 24), 1.08 * Math.pow(10, 12), 12704, 9.8, 147597870, 'azul', 'Sol', true)
+        const planeta2 = new Planeta('Marte', 6.41 * Math.pow(10, 23), 1.61 * Math.pow(10, 11), 6780, 3.72, 227936640, 'rojo', 'Sol', true)
+        console.log(planeta1);
+        planeta1.density()
+        
+        console.log('Densidad: ', planeta1.densidad);
+        console.log(planeta1);
+
+        selectOptions()
+    }
+
     salida(optionObject)
-    
+
 }
